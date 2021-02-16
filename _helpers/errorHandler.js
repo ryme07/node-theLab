e922bfbab4f0438a5da1caf02ba86933a83e257b
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 15:04:47 +0100

    feat: errorHandler

---
+++ _helpers/errorHandler.js
@@ -0,0 +1,41 @@
+const PORT = "8000";
+const app = require("../app");
+const server = require("../server");
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+const port = normalizePort(process.env.PORT || PORT);
+
+function errorHandler(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe" + address : "port" + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+module.exports = { errorHandler, normalizePort };
