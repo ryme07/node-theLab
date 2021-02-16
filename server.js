817d5839dab19ec3a44768aab7d8afe9b5c29b47
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 15:05:38 +0100

    feat: refacto server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const { errorHandler, normalizePort } = require("./_helpers/errorHandler");
 const http = require("http");
 const app = require("./app");
 const PORT = "8000";
 
-app.set("port", process.env.PORT || PORT);
+const port = normalizePort(process.env.PORT || PORT);
+app.set("port", port);
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || PORT);
-console.log(`Port listen - ${PORT}`);
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe" + address : "port" + port;
+  console.log("listening on " + bind);
+});
+
+server.listen(port);
