2252fe25eae2a4b90c40be4bb1945cc5903fb3b1
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 16:31:48 +0100

    feat: add put, delete & getById routes

--- routes/event.js
+++ routes/event.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const eventCtrl = require("../controllers/event");
 
 router.get("/", eventCtrl.getAllEvent);
+router.get("/:id", eventCtrl.getOneEvent);
 router.post("/", eventCtrl.createEvent);
+router.put("/:id", eventCtrl.modifyEvent);
+router.delete("/:id", eventCtrl.deleteEvent);
 
 module.exports = router;
