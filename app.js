9877513e8274f52dc8c8abf95490b983bf10bbf8
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 16:13:00 +0100

    feat: bodyParser & cors policy

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Event = require("./models/Event");
+const eventRoutes = require("./routes/event");
 
 //hide api Key
 dotenv.config();
 const apiKey = process.env.API_KEY;
+
+const app = express();
 
 const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose
@@ -12,10 +17,23 @@ mongoose
   .then(() => console.log("Connection to mongodb success"))
   .catch(() => console.log("connection failed"));
 
-const app = express();
-
-app.use((req, res) => {
-  res.json({ message: "Request done !" });
+//cors policy
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
+
+// body Parser
+app.use(bodyParser.json());
+
+app.use("/api/event", eventRoutes);
 
 module.exports = app;
