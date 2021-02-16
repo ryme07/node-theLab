2856aeff5f1d150b1d9783c5436b21a927cbcf4a
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 16:31:24 +0100

    feat: add put, delete & getById

--- controllers/event.js
+++ controllers/event.js
@@ -11,6 +11,24 @@ exports.createEvent = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
+exports.modifyEvent = (req, res, next) => {
+  Event.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Object edited !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.deleteEvent = (req, res, next) => {
+  Event.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "object delete !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneEvent = (req, res, next) => {
+  Event.findOne({ _id: req.params.id })
+    .then((event) => res.status(200).json(event))
+    .catch((error) => res.status(404).json({ error }));
+};
+
 exports.getAllEvent = (req, res, next) => {
   Event.find()
     .then((events) => res.status(200).json(events))
