a4ce89937cdadb5ac7b6ecf2a11157f51c0326e8
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 15:27:19 +0100

    feat: connexion with mongodb

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+//hide api Key
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose
+  .connect(apiKey, connectionOptions)
+  .then(() => console.log("Connection to mongodb success"))
+  .catch(() => console.log("connection failed"));
+
 const app = express();
 
 app.use((req, res) => {
