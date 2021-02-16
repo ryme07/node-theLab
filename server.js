10357587f707dacf147d868d296a21ece0d53227
ryme07 <remydelrieu@hotmail.fr>
2021-02-16 14:03:41 +0100

    feat: launch minimum server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const http = require("http");
+const app = require("./app");
+const PORT = "8000";
 
+app.set("port", process.env.PORT || PORT);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || PORT);
+console.log(`Port listen - ${PORT}`);
